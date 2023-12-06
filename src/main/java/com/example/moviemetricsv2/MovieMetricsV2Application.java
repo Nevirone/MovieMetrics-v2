bc31cdbf3eba62e5972f9613ad3eb0d15ab7a97a
Jakub Łachmański <jb.lachmanski@gmail.com>
2023-12-06 21:32:44 +0100

    Spring Initializr code

---
+++ src/main/java/com/example/moviemetricsv2/MovieMetricsV2Application.java
@@ -0,0 +1,13 @@
+package com.example.moviemetricsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMetricsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMetricsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviemetricsv2/MovieMetricsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviemetricsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMetricsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
